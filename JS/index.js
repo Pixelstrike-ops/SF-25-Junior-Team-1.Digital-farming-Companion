e4b0b93dc80a8aa35d7863c4c58dad660128e62a
Eloghosa Jeremiah Nosakhare <eloghosa.nosakhare@techskool.org>
2025-07-01 15:26:43 -0700

    Code Edit
    Edited styles.css, header.html, index.html, index.js, register.html

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,12 @@ function loadComponent(id, file){
     .then(res=>res.text())
     .then(data => {
         document.getElementById(id).innerHTML = data;
+        const links = document.querySelectorAll('#header nav div a');
+        links.forEach(link =>  {
+            if(link.href === window.location.href || link.pathname === window.location.pathname) {
+                link.classList.add('active');
+            }
+        })
     })
 }
 window.onload = () =>{
